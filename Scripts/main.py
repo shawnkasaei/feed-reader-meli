b8f5dcf6ce422483f5c16ef2b9e5ba38e7dfcbfe
Shawn Kasaei <shayankasaei@gmail.com>
2026-05-14 00:54:05 +0330

    update main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -100,17 +100,6 @@ class App:
             time.sleep(1)
 
         # ---------- BUILD UI ----------
-        feeds.sort(
-            key=lambda f: max(
-                (
-                    datetime.strptime(i.date, "%Y-%m-%d %H:%M:%S")
-                    for i in f["items"]
-                ),
-                default=datetime.min
-            ),
-            reverse=True
-        )
-
         html = self.html_builder.build(feeds)
 
         self.storage.save_html(html)
