9172edb8c5ac73490195d71334280669984d3e48
Shawn Kasaei <shayankasaei@gmail.com>
2026-06-01 16:39:51 +0330

    Update fetcher.py

--- Scripts/core/fetcher.py
+++ Scripts/core/fetcher.py
@@ -1,35 +1,44 @@
 import requests
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 class Fetcher:
 
     def __init__(self, timeout: int = 15):
        
         self.headers = {
-            "User-Agent": "Mozilla/5.0"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
         }
         self.timeout = timeout
 
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless=new")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument(f"user-agent={self.headers['User-Agent']}")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option("useAutomationExtension", False)
+
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+        self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source": "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+        })
+
+
     def get_text_by_selenium(self, url: str, css_selector_visibility_element:str="body") -> str:
-        
-        driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install())
-        )
+             
+        self.driver.get(url)
 
-        driver.get(url)
-
-        WebDriverWait(driver, self.timeout).until(
+        WebDriverWait(self.driver, self.timeout).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector_visibility_element))
         )
 
-        html = driver.page_source
-        
-        driver.quit()
+        html = self.driver.page_source
         
         return html
 
